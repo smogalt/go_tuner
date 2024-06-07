f5ee5ccdf03bd640071e833876f07a0de2d67c88
smogalt <107054973+smogalt@users.noreply.github.com>
2024-06-07 13:20:48 -0400

    Added default station list. Updated README. Added two column display and various keybinds

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 simple internet radio client inspired by pyradio (https://github.com/coderholic/pyradio). written in go mostly as a learning project but it is still very usable. 
 
 ### keybinds
+enter/space - select
+
 q - quit
+<<<<<<< HEAD
 
 up arrow - up on the list
 
@@ -11,9 +14,29 @@ down arrow - down on the list
 
 enter - select station
 
+=======
+>>>>>>> 9c4ac1c (Added default station list. Updated README. Added two column display and various keybinds)
 p - pause
+m - mute
+-/+ - vol up/down
+j/down arrow - down
+k/up arrow - up
 
 ## config
 
+<<<<<<< HEAD
 stations go in a csv file (~/.config/go_tuner/stations) no spaces
 (*i'll add a default file eventually*)
+=======
+stations go in a csv file (~/.config/go_tuner/stations) no spaces. *default file provided*
+
+name,url
+
+## dependencies
+- socat
+- mpv
+
+## install
+you can 'go run main.go' or build it to a binary (go build main.go). you can also use a precompiled binary copy the default stations list to .config/go_tuner/stations
+
+>>>>>>> 9c4ac1c (Added default station list. Updated README. Added two column display and various keybinds)
--- main.go
+++ main.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
 	"github.com/mattn/go-runewidth"
 	"github.com/nsf/termbox-go"
 	"os"
 	"os/exec"
 )
 
-type item struct {
+type station struct {
 	name string
 	url  string
 }
 
+type playlist struct {
+	stations     []station
+	selected     int
+	playing      int
+	volume       int
+	volume_human string
+}
+
 func main() {
+	/* playlist struct to hold the stations and which ones are playing */
+	var pl playlist
+
+	/* event queue for keyboard input */
 	eventQueue := make(chan termbox.Event)
 	go func() {
 		for {
@@ -21,74 +34,72 @@ func main() {
 		}
 	}()
 
-	home_dir, err := os.UserHomeDir()
+	/* init default station and load stations from ~/.config/go_tuner/stations */
+	pl.stations = load_stations()
+	pl.selected = 0
+	pl.playing = -1
+	pl.volume = 100
+	pl.volume_human = fmt.Sprint(100)
+
+	/* start termbox */
+	err := termbox.Init()
 	if err != nil {
 		panic(err)
 	}
 
-	stations_raw, err := os.Open(home_dir + "/.config/go_tuner/stations")
-	if err != nil {
-		panic(err)
-	}
-	defer stations_raw.Close()
-	csvReader := csv.NewReader(stations_raw)
-	data, err := csvReader.ReadAll()
-	if err != nil {
-		panic(err)
-	}
+	/* set screen */
+	refresh_screen(pl)
 
-	stations := parse_csv(data)
-
-	selected := 0
-	playing := -1
-
-	err = termbox.Init()
-	termbox.HideCursor()
-	if err != nil {
-		panic(err)
-	}
-
-	box()
-	print_menu(selected, stations)
-	termbox.Flush()
-
+	/* input loop */
 	for {
 		select {
 		case ev := <-eventQueue:
 			if ev.Type == termbox.EventKey {
 				switch {
-				case ev.Key == termbox.KeyArrowDown:
-					if selected == len(stations)-1 {
-						selected = -1
+				/* down in list */
+				case ev.Key == termbox.KeyArrowDown || ev.Ch == 'j':
+					if pl.selected == len(pl.stations)-1 {
+						pl.selected = 0
+					} else {
+						pl.selected++
 					}
-					selected++
-
-				case ev.Key == termbox.KeyArrowUp:
-					if selected == 0 {
-						selected = len(stations)
+				/* up in list */
+				case ev.Key == termbox.KeyArrowUp || ev.Ch == 'k':
+					if pl.selected == 0 {
+						pl.selected = len(pl.stations) - 1
+					} else {
+						pl.selected--
 					}
-					selected--
 
+				/* quit */
 				case ev.Ch == 'q':
 					quit()
 
+				/* pause */
 				case ev.Ch == 'p':
 					pause()
 
-				case ev.Key == termbox.KeyEnter:
-					if playing != selected {
-						play(stations[selected])
-						playing = selected
+				/* select station */
+				case ev.Key == termbox.KeyEnter || ev.Key == termbox.KeySpace:
+					play(&pl)
+					if pl.selected == len(pl.stations)-1 {
+						pl.selected = 0
+					} else {
+						pl.selected++
 					}
 
+				case ev.Ch == '-' || ev.Ch == '_':
+					vol(-1, &pl)
+
+				case ev.Ch == '=' || ev.Ch == '+':
+					vol(1, &pl)
+
+				case ev.Ch == 'm':
+					vol(0, &pl)
 				}
-
 			}
-			refresh()
-			print_menu(selected, stations)
-			termbox.Flush()
+			refresh_screen(pl)
 		}
-
 	}
 }
 
@@ -99,40 +110,76 @@ func tbprint(x, y int, fg, bg termbox.Attribute, msg string) {
 	}
 }
 
-func box() {
+func print_box(p playlist) {
 	w, h := termbox.Size()
+	/* horizontal sides */
 	for i := 1; i < w-1; i++ {
 		termbox.SetCell(i, 0, '─', termbox.ColorWhite, termbox.ColorDefault)
 		termbox.SetCell(i, h-1, '─', termbox.ColorWhite, termbox.ColorDefault)
 	}
 
+	/* vertical sides */
 	for j := 1; j < h-1; j++ {
 		termbox.SetCell(1, j, '│', termbox.ColorWhite, termbox.ColorDefault)
 		termbox.SetCell(w-2, j, '│', termbox.ColorWhite, termbox.ColorDefault)
 	}
 
+	/* corners */
 	termbox.SetCell(1, 0, '╭', termbox.ColorWhite, termbox.ColorDefault)
 	termbox.SetCell(w-2, 0, '╮', termbox.ColorWhite, termbox.ColorDefault)
 	termbox.SetCell(w-2, h-1, '╯', termbox.ColorWhite, termbox.ColorDefault)
 	termbox.SetCell(1, h-1, '╰', termbox.ColorWhite, termbox.ColorDefault)
 
-	tbprint(4, 0, termbox.ColorWhite, termbox.ColorDefault, " go tuner ")
+	/* title */
+	tbprint(4, 0, termbox.ColorWhite, termbox.ColorDefault, " go tuner "+"─"+" vol: "+p.volume_human+" ")
 }
 
-func print_menu(sel int, list [16]item) {
-	for i := 0; i < len(list); i++ {
-		if i == sel {
-			tbprint(4, (i*2)+2, termbox.ColorRed, termbox.ColorWhite, list[i].name)
+func print_menu(p playlist) {
+	x := 5
+	y := 2
+	w, h := termbox.Size()
+	for i := 0; i < len(p.stations); i++ {
+		y = ((i * 2) + 2) % (h - 4)
+
+		if ((i * 2) + 2) >= (h - 4) {
+			x = (w / 2) + 5
+			if h%2 == 0 {
+				y += 2
+			} else {
+				y++
+			}
+		}
+
+		if i == p.playing {
+			tbprint(x, y, termbox.ColorLightRed, termbox.ColorDefault, p.stations[i].name)
+			tbprint(x-2, y, termbox.ColorDarkGray, termbox.ColorDefault, "*")
 		} else {
-			tbprint(4, (i*2)+2, termbox.ColorRed, termbox.ColorDefault, list[i].name)
+			tbprint(x, y, termbox.ColorWhite, termbox.ColorDefault, p.stations[i].name)
+		}
+		if i == p.selected {
+			tbprint(x, y, termbox.ColorCyan, termbox.ColorDefault, p.stations[i].name)
+			tbprint(x-2, y, termbox.ColorLightCyan, termbox.ColorDefault, ">")
+		}
+		if i == p.selected && p.selected == p.playing {
+			tbprint(x, y, termbox.ColorCyan, termbox.ColorDefault, p.stations[i].name)
 		}
 	}
 }
 
-func refresh() {
+func print_help() {
+	_, h := termbox.Size()
+	tbprint(4, h-3, termbox.ColorDarkGray, termbox.ColorDefault, "k - up | j - down | enter/space - select")
+	tbprint(4, h-2, termbox.ColorDarkGray, termbox.ColorDefault, "m - mute | p - pause | q - quit | -/+ - vol up/down ")
+}
+
+func refresh_screen(p playlist) {
 	termbox.Clear(termbox.ColorWhite, termbox.ColorDefault)
-	box()
+	print_box(p)
+	print_menu(p)
+	print_help()
 	termbox.Flush()
+	w, h := termbox.Size()
+	termbox.SetCursor(w, h)
 }
 
 func quit() {
@@ -142,11 +189,17 @@ func quit() {
 	os.Exit(0)
 }
 
-func play(s item) {
-	cmd := exec.Command("/bin/sh", "-c", `echo '{"command": ["quit"]}' | socat - /tmp/mpvsocket`)
-	cmd.Run()
-	cmd = exec.Command("mpv", s.url, "--input-ipc-server=/tmp/mpvsocket")
-	cmd.Start()
+func play(p *playlist) {
+	if p.selected != p.playing {
+		cmd := exec.Command("/bin/sh", "-c", `echo '{"command": ["quit"]}' | socat - /tmp/mpvsocket`)
+		cmd.Run()
+		cmd = exec.Command("mpv", p.stations[p.selected].url, "--input-ipc-server=/tmp/mpvsocket")
+		cmd.Start()
+		p.playing = p.selected
+	} else {
+		pause()
+		return
+	}
 }
 
 func pause() {
@@ -154,18 +207,72 @@ func pause() {
 	cmd.Run()
 }
 
-func parse_csv(data [][]string) [16]item {
-	var menu_items [16]item
+/* true is up false is down */
+func vol(i int, p *playlist) {
+	if i == 1 && p.volume <= 95 {
+		p.volume += 5
+	}
+	if i == -1 && p.volume >= 5 {
+		p.volume -= 5
+	}
+	if i == 0 {
+		if p.volume_human == "muted" {
+			p.volume_human = fmt.Sprint(p.volume)
+		} else {
+			p.volume_human = "muted"
+		}
+
+		cmd := exec.Command("/bin/sh", "-c", `echo '{"command": ["cycle", "mute"]}' | socat - /tmp/mpvsocket`)
+		cmd.Run()
+		return
+	}
+	cmd := exec.Command("/bin/sh", "-c", `echo '{"command": ["set_property", "volume", `+fmt.Sprint(p.volume)+`]}' | socat - /tmp/mpvsocket`)
+	cmd.Run()
+
+	if p.volume == 0 {
+		p.volume_human = "muted"
+	} else {
+		p.volume_human = fmt.Sprint(p.volume)
+	}
+}
+
+func load_stations() []station {
+	home_dir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+
+	stations_raw, err := os.Open(home_dir + "/.config/go_tuner/stations")
+	if err != nil {
+		cmd := exec.Command("mkdir", "-p", home_dir+"/.config/go_tuner/")
+		cmd.Run()
+		cmd = exec.Command("touch", home_dir+"/.config/go_tuner/stations")
+		cmd.Run()
+	}
+
+	csvReader := csv.NewReader(stations_raw)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	var menu_items []station
+
 	for i, line := range data {
-		if i < len(menu_items) {
+		if i > 0 {
+			var temp station
 			for j, field := range line {
 				if j == 0 {
-					menu_items[i].name = field
+					temp.name = field
 				} else if j == 1 {
-					menu_items[i].url = field
+					temp.url = field
 				}
 			}
+			menu_items = append(menu_items, temp)
 		}
 	}
+
+	stations_raw.Close()
+
 	return menu_items
 }
